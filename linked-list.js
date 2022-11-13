899435f67aa26b12a632e5b4284612ae5bbdf408
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-11-13 21:05:16 +0100

    added comments

--- linked-list.js
+++ linked-list.js
@@ -6,35 +6,35 @@ class LinkedList {
     append(value) {
         let node = new Node(value, null)
         
-        if (!this.head) {
+        if (!this.head) { // Set head to node 
             this.head = node
             return this.head
         } 
-        let tail = this.head
-        while(tail.nextNode !== null) {
-            tail = tail.nextNode
+        let tail = this.head // tail is the pointer for the last value, start at head
+        while(tail.nextNode !== null) { // the last element will have a null nextNode, let's use that!
+            tail = tail.nextNode // iterates
         }
-        tail.nextNode = node
+        tail.nextNode = node // when found append new node to nextNode of last node
         return this.head   
     }
 
     prepend(value) {
         let node = new Node(value, this.head.nextNode)
-        let temp = this.head
+        let temp = this.head // "slice" the structure by saving head to temp
         if (this.head) {
-            this.head = node
+            this.head = node // replace original head with new node
         }
-        this.head.nextNode = temp
+        this.head.nextNode = temp // replace new node's nextNode with the original head node
     }
 
     size() {
         let pointer = this.head;
         let size = 0;
-        while(pointer.nextNode !== null) {
-            size++
+        while(pointer.nextNode !== null) { // same principle as append
+            size++ // count interations
             pointer = pointer.nextNode;
         }
-        return size;
+        return size; // number of iterations is equal to the size of the list!
     }
 
     heads() {
@@ -43,7 +43,7 @@ class LinkedList {
 
     tails() {
         let pointer = this.head
-        while(pointer.nextNode !== null) {
+        while(pointer.nextNode !== null) { // same as append and size
             pointer = pointer.nextNode
         }
         return pointer
